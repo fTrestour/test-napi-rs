a8eeb6eedfff1d74f8350506c059fad3825aa08c
Fabien <ftrestour@gmail.com>
2023-07-30 17:06:19 +0200

    ⚗️ Benchmarking various object sizes

--- mircobenchmark.js
+++ mircobenchmark.js
@@ -1,20 +1,26 @@
 const { parse: parseRust } = require("./index");
 const { parseJs } = require("./parse-js");
 
-const benchmark = (name, f, size) => {
+const build_data = (size) => `{
+    "name": "John Doe",
+    "phones": [
+        ${Array.from({ length: size }, () => '"+33 123456789"').join(",\n")}
+    ]
+  }`;
+
+const benchmark = (name, f, iterations, size) => {
   console.time(name);
 
-  for (let i = 0; i < size; i++) {
-    f(`{
-        "name": "John Doe",
-        "phones": [
-            "+33 123456789"
-        ]
-      }`);
+  for (let i = 0; i < iterations; i++) {
+    f(build_data(size));
   }
 
   console.timeEnd(name);
 };
 
-benchmark("JS", parseJs, 1000000);
-benchmark("Rust", parseRust, 1000000);
+const iterations = 1000;
+const sizes = Array.from({ length: 6 }, (_, i) => 10 ** i);
+sizes.forEach((size) => {
+  benchmark(`JS, ${size}`, parseJs, iterations, size);
+  benchmark(`Rust, ${size}`, parseRust, iterations, size);
+});
